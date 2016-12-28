e6ab10167c4a25eca50b57b564e25851218b3e81
Kirill Morozov <kirillmorozov@users.noreply.github.com>
2016-12-28 13:49:48 +0300

    reformat docstrings

--- Python/Data Types/tuples.py
+++ Python/Data Types/tuples.py
@@ -1,24 +1,43 @@
 #!/usr/bin/env python
 """
+====
 Task
+====
 You are given an integer, N, on a single line. The next line contains N
 space-separated integers. Create a tuple, T, of those N integers, then compute
 and print the result of hash(T).
 Note: hash() is one of the functions in the __builtins__ module.
 
+============
 Input Format
+============
 The first line contains an integer, N (the number of elements in the tuple).
-The second line contains NN space-separated integers describing T.
+The second line contains N space-separated integers describing T.
 
+=============
 Output Format
+=============
 Print the result of hash(T).
 
+============
 Sample Input
-2
-1 2
+============
+Input::
+    2
+    1 2
 
+=============
 Sample Output
-3713081631934410656
+=============
+Output::
+    3713081631934410656
+
+=========
+Editorial
+=========
+Solution::
+    n = raw_input()
+    print hash(tuple([int(i) for i in raw_input().split()]))
 """
 
 from __future__ import print_function, unicode_literals
