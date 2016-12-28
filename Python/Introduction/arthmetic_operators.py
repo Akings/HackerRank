49a56a7da05fe4ccf56a4d505977afee9d4a9aba
Kirill Morozov <kirillmorozov@users.noreply.github.com>
2016-12-28 10:59:33 +0300

    reformat docstrings

--- Python/Introduction/arthmetic_operators.py
+++ Python/Introduction/arthmetic_operators.py
@@ -1,45 +1,49 @@
 #!/usr/bin/env python
 """
-Let's learn about Python's arithmetic operators.
-
-First, let's read two integers:
-a = int(raw_input())
-b = int(raw_input())
-
-The three basic arithmetic operators are the following:
-Addition (+)
-Subtraction (-)
-Multiplication (*)
-(We'll learn about division in the next task.)
-
+====
 Task
+====
 Read two integers from STDIN and print three lines where:
 
 1. The first line contains the sum of the two numbers.
 2. The second line contains the difference of the two numbers (first - second).
 3. The third line contains the product of the two numbers.
 
+============
 Input Format
+============
 The first line contains the first integer, a. The second line contains the
 second integer, b.
 
+===========
 Constraints
+===========
 1≤a≤10^10
 1≤b≤10^10
 
+=============
 Output Format
+=============
 Print the three lines as explained above.
 
+============
 Sample Input
-3
-2
+============
+Input::
+    3
+    2
 
+=============
 Sample Output
-5
-1
-6
+=============
+Output::
+    5
+    1
+    6
 
+===========
 Explanation
+===========
 3+2⟹5
 3−2⟹1
 3∗2⟹6
