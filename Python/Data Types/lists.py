4a47341e27a2e2e0c8020b096bb7bed43463f939
Kirill Morozov <kirillmorozov@users.noreply.github.com>
2016-12-28 11:38:01 +0300

    reformat docstings

--- Python/Data Types/lists.py
+++ Python/Data Types/lists.py
@@ -1,61 +1,75 @@
 #!/usr/bin/env python
 """
+====
 Task
+====
 Initialize your list (L = []) and follow the N commands given over N lines.
 Each command will be 1 of the 8 commands given above. The extend(L) method
 will not be used. Each command will have its own value(s) separated by a space.
 
+============
 Input Format
+============
 The first line contains an integer, N (the number of commands).
 The N subsequent lines each contain one of the 8 commands described above.
 
+============
 Sample Input
-12
-insert 0 5
-insert 1 10
-insert 0 6
-print
-remove 6
-append 9
-append 1
-sort
-print
-pop
-reverse
-print
+============
+Input::
+    12
+    insert 0 5
+    insert 1 10
+    insert 0 6
+    print
+    remove 6
+    append 9
+    append 1
+    sort
+    print
+    pop
+    reverse
+    print
 
+=============
 Sample Output
-[6, 5, 10]
-[1, 5, 9, 10]
-[9, 5, 1]
+=============
+Output::
+    [6, 5, 10]
+    [1, 5, 9, 10]
+    [9, 5, 1]
 
-Editorial:
-arr = []
-for i in range(int(raw_input())):
-    s = raw_input().split()
-    for i in range(1,len(s)):
-        s[i] = int(s[i])
+=========
+Editorial
+=========
+Solution::
 
-    if s[0] == "append":
-        arr.append(s[1])
-    elif s[0] == "extend":
-        arr.extend(s[1:])
-    elif s[0] == "insert":
-        arr.insert(s[1],s[2])
-    elif s[0] == "remove":
-        arr.remove(s[1])
-    elif s[0] == "pop":
-        arr.pop()
-    elif s[0] == "index":
-        print arr.index(s[1])
-    elif s[0] == "count":
-        print arr.count(s[1])
-    elif s[0] == "sort":
-        arr.sort()
-    elif s[0] == "reverse":
-        arr.reverse()
-    elif s[0] == "print":
-        print arr
+    arr = []
+    for i in range(int(raw_input())):
+        s = raw_input().split()
+        for i in range(1,len(s)):
+            s[i] = int(s[i])
+
+        if s[0] == "append":
+            arr.append(s[1])
+        elif s[0] == "extend":
+            arr.extend(s[1:])
+        elif s[0] == "insert":
+            arr.insert(s[1],s[2])
+        elif s[0] == "remove":
+            arr.remove(s[1])
+        elif s[0] == "pop":
+            arr.pop()
+        elif s[0] == "index":
+            print arr.index(s[1])
+        elif s[0] == "count":
+            print arr.count(s[1])
+        elif s[0] == "sort":
+            arr.sort()
+        elif s[0] == "reverse":
+            arr.reverse()
+        elif s[0] == "print":
+            print arr
 """
 
 from __future__ import print_function, unicode_literals
--- Python/Data Types/sets_symmetric_difference.py
+++ Python/Data Types/sets_symmetric_difference.py
@@ -1,35 +1,50 @@
 #!/usr/bin/env python
 """
+====
 Task
+====
 Given 2 sets of integers, M and N, print their symmetric difference in
 ascending order. The term symmetric difference indicates those values that
 exist in either M or N but do not exist in both.
 
+============
 Input Format
+============
 The first line of input contains an integer, M.
 The second line contains M space-separated integers.
 The third line contains an integer, N.
 The fourth line contains N space-separated integers.
 
+=============
 Output Format
+=============
 Output the symmetric difference integers in ascending order, one per line.
 
+============
 Sample Input
-4
-2 4 5 9
-4
-2 4 11 12
+============
+Input::
+    4
+    2 4 5 9
+    4
+    2 4 11 12
 
+=============
 Sample Output
-5
-9
-11
-12
+=============
+Output::
+    5
+    9
+    11
+    12
 
+=========
 Editorial
-M = raw_input();m = raw_input().split()
-N = raw_input();n = raw_input().split()
-print "\n".join(sorted(list(set(m) ^ set(n)),key=int))
+=========
+Solution::
+    M = raw_input();m = raw_input().split()
+    N = raw_input();n = raw_input().split()
+    print "\n".join(sorted(list(set(m) ^ set(n)),key=int))
 """
 
 from __future__ import print_function, unicode_literals
