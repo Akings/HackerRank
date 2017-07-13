90cefbec670f1aa0a2ee1b2aaf7a7b7e82b4e27a
Kirill Morozov <k.morozov@robotics.guru>
2017-07-13 18:32:09 +0300

    remove module's docstings and python2 support

--- Python/Introduction/division.py
+++ Python/Introduction/division.py
@@ -1,37 +1,4 @@
-#!/usr/bin/env python
-"""
-====
-Task
-====
-Read two integers and print two lines. The first line should contain integer
-division, a//b. The second line should contain float division, a/b.
-
-You don't need to perform any rounding or formatting operations.
-
-============
-Input Format
-============
-The first line contains the first integer, aa. The second line contains the
-second integer, bb.
-
-=============
-Output Format
-============
-Print the two lines as described above.
-============
-Sample Input
-============
-Input::
-    4
-    3
-
-=============
-Sample Output
-=============
-Output::
-    1
-    1.3333333333333333
-"""
+#!/usr/bin/env python3
 
 
 def main():
--- Python/Introduction/loops.py
+++ Python/Introduction/loops.py
@@ -1,45 +1,4 @@
-#!/usr/bin/env python2
-"""
-====
-Task
-====
-Read an integer N. For all non-negative integers i<N, print i^2. See the
-sample for details.
-
-============
-Input Format
-============
-The first and only line contains the integer, N.
-
-===========
-Constraints
-===========
-1≤N≤20
-
-=============
-Output Format
-=============
-Print N lines, one corresponding to each i.
-
-============
-Sample Input
-============
-Input::
-    5
-
-=============
-Sample Output
-=============
-Output::
-    0
-    1
-    4
-    9
-    16
-"""
-
-
-from __future__ import print_function
+#!/usr/bin/env python3
 
 
 def main():
